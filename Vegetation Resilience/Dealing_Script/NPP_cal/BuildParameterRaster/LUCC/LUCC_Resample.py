10e220bf2773c7fd1154632ba5fa76c82aadda8f
PZH <escape_matser@outlook.com>
2024-08-23 18:39:59 +0800

    construct specific lucc

--- Vegetation Resilience/Dealing_Script/NPP_cal/BuildParameterRaster/LUCC/LUCC_Resample.py
+++ Vegetation Resilience/Dealing_Script/NPP_cal/BuildParameterRaster/LUCC/LUCC_Resample.py
@@ -1,23 +1,23 @@
-from multiprocessing import Pool
-
 from osgeo import gdal
 import os
+
+from tqdm import tqdm
 
 from UtilitiesForDealingImage.ImageProcessing import resample_image
 from UtilitiesForDealingImage.UtilityFunction import workdir_filelist
 
 
-def para_resample(input_path, output_dir, width, height):
-    try:
-        in_ds = gdal.Open(input_path)
-        if in_ds is None:
-            raise ValueError(f"Failed to open {input_path}")
-
-        resample_image(in_ds, width, height, output_dir=output_dir,
-                       return_ds=False, reproject_method=gdal.GRA_NearestNeighbour, input_file_path=input_path)
-        del in_ds
-    except Exception as a:
-        print(f"Error processing {input_path}: {a}")
+# def para_resample(input_path, output_dir, width, height):
+#     try:
+#         in_ds = gdal.Open(input_path)
+#         if in_ds is None:
+#             raise ValueError(f"Failed to open {input_path}")
+#
+#         resample_image(in_ds, width, height, output_dir=output_dir,
+#                        return_ds=False, reproject_method=gdal.GRA_NearestNeighbour, input_file_path=input_path)
+#         del in_ds
+#     except Exception as a:
+#         print(f"Error processing {input_path}: {a}")
 
 
 if __name__ == "__main__":
@@ -30,12 +30,19 @@ if __name__ == "__main__":
     ref_ds: gdal.Dataset = gdal.Open(ref)
     width = ref_ds.RasterXSize
     height = ref_ds.RasterYSize
+    ref_trans = ref_ds.GetGeoTransform()
     del ref_ds
-    ls = [(filelist[i], out_dir, width, height) for i in range(len(filelist))]
+    # ls = [(filelist[i], out_dir, width, height) for i in range(len(filelist))]
     # resample_image(*ls)
     try:
-        with Pool(processes=1) as pool:
-            results = pool.starmap(para_resample, ls)
+        # with Pool(processes=1) as pool:
+        #     results = pool.starmap(para_resample, ls)
+        for file in tqdm(filelist):
+            in_ds = gdal.Open(file)
+            resample_image(in_ds, width, height, output_dir=out_dir,
+                           return_ds=False, reproject_method=gdal.GRA_NearestNeighbour,
+                           input_file_path=file, reference_transform=ref_trans)
+            del in_ds
     except Exception as e:
         print("something went wrong")
         raise
